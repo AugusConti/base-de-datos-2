0c7bfab6c7ffa21b00640104eb5b45136017cc03
Tomás Mercante <tomiimercante@gmail.com>
2025-05-09 11:10:49 -0300

    Cambios en los repos de usuarios

--- src/main/java/unlp/info/bd2/repositories/DriverUserRepository.java
+++ src/main/java/unlp/info/bd2/repositories/DriverUserRepository.java
@@ -1,15 +1,19 @@
 package unlp.info.bd2.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
 import unlp.info.bd2.model.DriverUser;
 
-public interface DriverUserRepository extends BaseUserRepository<DriverUser> {
+public interface DriverUserRepository extends CrudRepository<DriverUser, Long> {
+    
+    Optional<DriverUser> findByUsername(String username);
 
-    @Query("FROM DriverUser du JOIN du.routes r GROUP BY du.id ORDER BY COUNT(r) DESC")
+    @Query("FROM DriverUser du ORDER BY SIZE(du.routes) DESC")
     List<DriverUser> findAllSortByRouteCountDesc(Pageable pageable);
 
 }
--- src/main/java/unlp/info/bd2/repositories/UserRepository.java
+++ src/main/java/unlp/info/bd2/repositories/UserRepository.java
@@ -1,23 +1,28 @@
 package unlp.info.bd2.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import unlp.info.bd2.model.User;
 
 @Repository
-public interface UserRepository extends BaseUserRepository<User> {
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
     @Query("SELECT DISTINCT u FROM User u JOIN u.purchaseList p WHERE p.totalPrice >= :mount")
     List<User> findByMountSpendingGreaterThan(@Param("mount") float mount);
 
-    // TODO CONUSLTAR Formato de queries (de otra forma tira error)
-    @Query("SELECT u FROM User u JOIN u.purchaseList p GROUP BY u.id HAVING COUNT(p) >= :number")
+    @Query("SELECT u FROM User u WHERE SIZE(u.purchaseList) >= :number")
     List<User> findByPurchaseCountGreaterThan(@Param("number") int number);
 
-    @Query("SELECT u FROM User u JOIN u.purchaseList p GROUP BY u.id ORDER BY COUNT(p) DESC")
+    @Query("SELECT u FROM User u ORDER BY SIZE(u.purchaseList) DESC")
     List<User> findAllSortByPurchaseCountDesc(Pageable pageable);
 }
